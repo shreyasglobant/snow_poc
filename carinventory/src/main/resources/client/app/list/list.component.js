a85f1ce68739b4560ee9406244fc2c6c0022f50b
mitesh.neema <mitesh.neema@globant.com>
2017-04-26 12:08:14 +0530

    Merge branch 'master' of https://github.com/shreyasglobant/snow_poc
    # Conflicts:
    #	carinventory/src/main/resources/client/app/index.ejs
    #	carinventory/src/main/resources/client/app/login/login.component.js
    #	carinventory/src/main/resources/client/app/login/login.html

--- carinventory/src/main/resources/client/app/list/list.component.js
+++ carinventory/src/main/resources/client/app/list/list.component.js
@@ -14,35 +14,35 @@ let listComponent = {
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
         when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
+        notes: 'I will be in your neighborhood doing errands'
       },
       {
         face : imagePath,
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
         when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
+        notes: 'I will be in your neighborhood doing errands'
       },
       {
         face : imagePath,
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
         when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
+        notes: 'I will be in your neighborhood doing errands'
       },
       {
         face : imagePath,
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
         when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
+        notes: 'I will be in your neighborhood doing errands'
       },
       {
         face : imagePath,
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
         when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
+        notes: 'I will be in your neighborhood doing errands'
       }
     ];
   }
