92211a6abcc5ba01b160f134da92c19632308682
Jayant Patil <jayant.patil@globant.com>
2017-04-27 19:24:26 +0530

    Api integarted for car addtion

--- carinventory/src/main/resources/client/app/env/env.constant.js
+++ carinventory/src/main/resources/client/app/env/env.constant.js
@@ -1,6 +1,7 @@
 const env = {
   baseUrl : '/api/',
   loginApi : '31016/authentications/login',
+  addCarApi: 'api/now/table/u_car',
   auth : {key: 'authorization', token: 'Basic TWFuZGFyLkthbXRla2FyOjEyMzQ1'}
 
 }
--- carinventory/src/main/resources/client/app/newCar/newCar.service.js
+++ carinventory/src/main/resources/client/app/newCar/newCar.service.js
@@ -1,12 +1,34 @@
-function newCarService($state) {
+function newCarService($state, sessionService, $http, ENV, $q) {
   var cars = [];
   return {
     cars: [],
     title: () => $state.current.name,
+    getCars: () => cars,
     addCar: (car) => {
-      cars.push(car);
-    },
-    getCars: () => cars
+      return $q((resolve, reject) => {
+        $http({
+          method: 'POST',
+          url: `${ENV.addCarApi}`,
+          data: [
+            {
+              'u_serial': car.u_serial,
+              'u_manufacturer': car.u_manufacturer,
+              'u_segment': car.u_segment,
+              'u_model': car.u_model,
+              'u_quantity': car.u_quanity
+            }
+          ]
+        }).then(res => {
+          if (res) {
+            resolve(true);
+          }
+          else {
+            resolve(false);
+          }
+        })
+        .catch(reject);
+      });
+    }
   }
 
 }
